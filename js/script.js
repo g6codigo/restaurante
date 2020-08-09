8ed7fa9d4d4e1d6b0752a8565f82188645f78289
raquel <rhnoriega@outlook.com>
2020-08-09 17:34:40 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+filterSelection("all") // Execute the function and show all columns
+function filterSelection(c) {
+  
+  let x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (let i = 0; i < x.length; i++) {
+    removerClase(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) agregarClase(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function agregarClase(element, name) {
+  let arr1 = element.className.split(" ");
+  let arr2 = name.split(" ");
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function removerClase(element, name) {
+  let arr1 = element.className.split(" ");
+  let arr2 = name.split(" ");
+  console.log(arr2)
+  for (let i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+//menu slider
+
+var header = $('header');
+var range = 200;
+
+$(window).on('scroll', function () {
+  
+  var scrollTop = $(this).scrollTop(),
+      height = header.outerHeight(),
+      offset = height / 2,
+      calc = 1 - (scrollTop - offset + range) / range;
+
+  header.css({ 'opacity': calc });
+
+  if (calc > '1') {
+    header.css({ 'opacity': 1 });
+  } else if ( calc < '0' ) {
+    header.css({ 'opacity': 0 });
+  }
+  
+});
